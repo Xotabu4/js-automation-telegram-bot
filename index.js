bfef453161f26e553534b93142c98d35949599ac
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2023-06-06 16:28:05 +0300

    New bot!

--- index.js
+++ index.js
@@ -1,71 +1,121 @@
 const TelegramBot = require("node-telegram-bot-api");
-const spawn = require("child-process-promise").spawn;
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
-const BOT_NAME = "@JSAutomationGroupBot";
+const BOT_NAME = "@HotTestingBot";
+
+const HOT_TESTING_CHANNEL_ID = -1001465526599;
+const HOT_TESTING_CHAT_ID = -1001144433746;
+const MESSAGE_NEWS_THREAD_ID = 113567;
 
 const options = {
   // Use pooling for local debug only.
-  // polling: true
+  // polling: true,
   webHook: {
     // Port to which you should bind is assigned to $PORT variable
     // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
     port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
-// Heroku routes from port :443 to $PORT
-// Add URL of your app to env variable or enable Dyno Metadata
-// to get this automatically
-// See: https://devcenter.heroku.com/articles/dyno-metadata
 const url =
-  process.env.APP_URL || "https://js-automation-telegram-bot.herokuapp.com:443";
+  process.env.APP_URL || "https://hot-testing-telegram-bot.herokuapp.com:443";
 const bot = new TelegramBot(TELEGRAM_TOKEN, options);
 
-// This informs the Telegram servers of the new webhook.
-// Note: we do not need to pass in the cert, as it already provided
 bot.setWebHook(`${url}/bot${TELEGRAM_TOKEN}`);
 
-// Sending message to myself on start, disable if to much spam
-//bot.sendMessage(121956343, `My master, i am started at ${new Date()}`);
+bot.on('channel_post', ChanelPostCallback)
 
-bot.onText(/\/execute .*/, async (msg, match) => {
-  console.log("msg", msg);
-  console.log("match", match);
+/**
+ * @param {TelegramBot.Message} msg 
+ */
+async function ChanelPostCallback(msg) {
+  console.log(JSON.stringify(msg, null, 2));
+  if (msg.chat.id !== HOT_TESTING_CHANNEL_ID) {
+    // Avoiding people to add this bot somewhere else.
+    return;
+  }
 
-  let toExecute = match.input.replace("/execute ", "");
-  // Cleaning quotemarks autoreplacement
-  toExecute = toExecute.replace("‘", "'");
-  toExecute = toExecute.replace("’", "'");
+  // to, from, message_id
+  await bot.forwardMessage(HOT_TESTING_CHAT_ID, HOT_TESTING_CHANNEL_ID, msg.message_id, {
+    disable_notification: true,
+    disable_web_page_preview: false,
+  });
 
-  console.log("Code to execute: ", toExecute);
-  // Timeout for long running commands
-  const timeoutMarker = setTimeout(function() {
-    bot.sendMessage(
-      msg.chat.id,
-      `Sorry, your code reached 1 min timeout: ${toExecute}`
-    );
-    childProcess.kill();
-  }, 60000);
-  const processPromise = spawn(
-    "node",
-    [`${__dirname}/executor.js`, "--code", Buffer.from(toExecute)],
-    {
-      capture: ["stdout", "stderr"]
-    }
-  )
-    .then(function(result) {
-      clearTimeout(timeoutMarker);
-      console.log("[spawn] stdout: ", result.stdout.toString());
-      bot.sendMessage(msg.chat.id, "Output: " + result.stdout.toString());
-    })
-    .catch(function(err) {
-      clearTimeout(timeoutMarker);
-      console.error("[spawn] stderr: ", err.stderr);
-      bot.sendMessage(msg.chat.id, "Error output: " + err.stderr.toString());
-    });
-  const childProcess = processPromise.childProcess;
-  console.log("[spawn] New process spawned: ", childProcess.pid);
-});
+//   await bot.sendMessage(HOT_TESTING_CHAT_ID, 
+// `https://t.me/Javascript_testing_channel/${msg.message_id}`, {
+//     reply_to_message_id: MESSAGE_NEWS_THREAD_ID,
+//     disable_notification: true,
+//     disable_web_page_preview: false,
+//   })
+}
+
+// Post in channel:
+// https://t.me/Javascript_testing_channel/720
+const exampleChannelPost = {
+  "message_id": 723,
+  "author_signature": "Alex Hot",
+  "sender_chat": {
+    "id": -1001465526599,
+    "title": "Hot testing Channel",
+    "username": "Javascript_testing_channel",
+    "type": "channel"
+  },
+  "chat": {
+    "id": -1001465526599,
+    "title": "Hot testing Channel",
+    "username": "Javascript_testing_channel",
+    "type": "channel"
+  },
+  "date": 1686057114,
+  "text": "test2"
+};
+
+
+// Message in thread:
+// https://t.me/js_for_testing/113551/113567
+const exampleThread = {
+  "message_id": 113563,
+  "from": {
+    "id": 121956343,
+    "is_bot": false,
+    "first_name": "Alex",
+    "last_name": "Hot",
+    "username": "xotabu4",
+    "language_code": "en"
+  },
+  "chat": {
+    "id": -1001144433746,
+    "title": "Hot testing",
+    "username": "js_for_testing",
+    "is_forum": true,
+    "type": "supergroup"
+  },
+  "date": 1686047163,
+  "message_thread_id": 113551,
+  "reply_to_message": {
+    "message_id": 113551,
+    "from": {
+      "id": 121956343,
+      "is_bot": false,
+      "first_name": "Alex",
+      "last_name": "Hot",
+      "username": "xotabu4",
+      "language_code": "en"
+    },
+    "chat": {
+      "id": -1001144433746,
+      "title": "Hot testing",
+      "username": "js_for_testing",
+      "is_forum": true,
+      "type": "supergroup"
+    },
+    "date": 1686043165,
+    "message_thread_id": 113551,
+    "forum_topic_created": {
+      "name": "News",
+      "icon_color": 7322096
+    },
+    "is_topic_message": true
+  },
+  "text": "test",
+  "is_topic_message": true
+}
